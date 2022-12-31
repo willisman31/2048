6b96192b8fe3bed65cea9109e919776a821d69ca
jacob <willisjp@dukes.jmu.edu>
2022-12-31 00:11:58 -0500

    Update

--- Game.java
+++ Game.java
@@ -1,11 +1,18 @@
 public class Game {
+
+	private Board board;
+	int boardSize = Board.DEFAULT_SIZE;
+	private boolean useGui = true;
+
 	public Game() {
 		// play game
 		// set game configs here- board size, special value adds, time constraints, etc.
+		this.board = new Board(boardSize);
 	}
 
 	public Game(boolean gui) {
-		if (gui) super();
+		this.useGui = gui;
+		if (useGui) super();
 		else {
 			// play game with no gui
 			// set game configs here
@@ -14,10 +21,29 @@ public class Game {
 
 	public void play() {
 		// play game with gui
+		while(Board.hasPossibleMove()) {
+			// TODO
+		}
 	}
 
 	public void playNoGui() {
 		// play with no gui
+	}
+
+	public void setBoardSize(int sideLength) {
+		this.boardSize = sideLength;
+	}
+
+	public int getBoardSize() {
+		return this.boardSize;
+	}
+
+	public boolean useGui() {
+		return this.useGui;
+	}
+
+	public void setUseGui(boolean gui) {
+		this.useGui = gui;
 	}
 }
 
